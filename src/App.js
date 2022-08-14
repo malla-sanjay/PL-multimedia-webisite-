316b899a86d67156721a6d6c4c6b7cc515224b0e
Sanjay Malla <mallasanjay44@gmail.com>
2022-08-14 07:30:43 +0545

    initial service commit (service card complete)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// this is the service card component.
+// it is basically a function which takes logo,title,& description parameters to generate a service card.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+
+function App (props) {
+  return(
+    <div className="serviceCard">
+      <img className="logo" src={props.logo} alt="services logo"></img>
+      <div className="aboutLogo">
+        <h2>{props.title}</h2>
+        <h3>{props.description}</h3>
+      </div>
+      <button className="contactButton">Click to Contact</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./serviceCard.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App logo={require("./logo/seo.png")} title="this is title" description="lorem20 is not working on my vs code so i am randomly typing this bullshit."/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
