0df8a7ec69b6ec8c445a15ca35b44bbb796e799e
Sanjay Malla <mallasanjay44@gmail.com>
2022-08-18 10:30:20 +0545

    Updates

---
+++ precache-manifest.c88d4b8de65e679d5f51fc438eb8c87f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b92a83599e4bf0b81b28",
+    "url": "/PL-multimedia-webisite-/static/css/main.a378d2b7.chunk.css"
+  },
+  {
+    "revision": "b92a83599e4bf0b81b28",
+    "url": "/PL-multimedia-webisite-/static/js/main.b92a8359.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/PL-multimedia-webisite-/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "78c828c6d891b4ab7546",
+    "url": "/PL-multimedia-webisite-/static/js/runtime~main.78c828c6.js"
+  },
+  {
+    "revision": "3701a95a0de2136bdf17dd9796d0f297",
+    "url": "/PL-multimedia-webisite-/static/media/www.3701a95a.png"
+  },
+  {
+    "revision": "b9e3c20ea2e2320c9c51488b61e9f38c",
+    "url": "/PL-multimedia-webisite-/static/media/webDevelopment.b9e3c20e.png"
+  },
+  {
+    "revision": "5e154ec19571cc49e0716f422db3816e",
+    "url": "/PL-multimedia-webisite-/static/media/webHosting.5e154ec1.png"
+  },
+  {
+    "revision": "3d01aed438401db9505c4f88de13c373",
+    "url": "/PL-multimedia-webisite-/static/media/seo.3d01aed4.png"
+  },
+  {
+    "revision": "28e4a729b02419e8fa0d6c3df89423c7",
+    "url": "/PL-multimedia-webisite-/static/media/digitalMarketing.28e4a729.png"
+  },
+  {
+    "revision": "d4a338f35153ca7d4a0190068c5705d3",
+    "url": "/PL-multimedia-webisite-/index.html"
+  }
+];
